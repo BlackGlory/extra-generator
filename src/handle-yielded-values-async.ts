ab8529d81381962b5bf37f139ee8ca44b3826267
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-20 05:26:50 +0000

    docs: add comments

--- src/handle-yielded-values-async.ts
+++ src/handle-yielded-values-async.ts
@@ -14,7 +14,11 @@ export async function handleYieldedValuesAsync<T, Return, Next>(
 
     return value as Return
   } catch (e) {
+    // 如果generator没有捕获错误, 则调用`generator.throw`就会抛出错误.
+    // 如果generator捕获了错误, 则调用`generator.throw`会返回`{ done: true }`.
     generator.throw(e)
+
+    // 为了确保函数能在抛出错误时失败, 总是在此处重新抛出错误.
     throw e
   }
 }
