33684b0d5e37b0d32fe36101e6285e68fe6f2f48
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-17 17:53:12 +0000

    feat(ngrams): add parameter validators

--- __tests__/ngrams.spec.ts
+++ __tests__/ngrams.spec.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest'
 import { ngrams } from '@src/ngrams.js'
 import { toArray } from 'iterable-operator'
+import { getError } from 'return-style'
 
 describe('ngrams', () => {
   test('1-gram', () => {
@@ -17,5 +18,11 @@ describe('ngrams', () => {
     const result = toArray(iter)
 
     expect(result).toStrictEqual(['ab', 'bc'])
+  })
+
+  test('edge: 0-gram', () => {
+    const err = getError(() => ngrams('abc', 0))
+
+    expect(err).toBeInstanceOf(Error)
   })
 })
--- src/ngrams.ts
+++ src/ngrams.ts
@@ -1,4 +1,16 @@
-export function* ngrams(text: string, n: number): IterableIterator<string> {
+import { assert } from '@blackglory/prelude'
+
+export function ngrams(text: string, n: number): IterableIterator<string> {
+  assert(
+    Number.isInteger(n) &&
+    n > 0
+  , 'The parameter n must be an integer and greater than zero'
+  )
+
+  return _ngrams(text, n)
+}
+
+function* _ngrams(text: string, n: number): Generator<string> {
   for (let i = 0; i < text.length; i++) {
     const result = text.slice(i, i + n)
     if (result.length === n) {
